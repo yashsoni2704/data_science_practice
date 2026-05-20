da69b5195558ea5ef74c939ecaa5920b8e8339dc
Yash Soni <yashrakeshsoni@gmail.com>
2026-05-20 12:31:29 +0530

    no

--- Part_4/Q4.py
+++ Part_4/Q4.py
@@ -1,1 +1,20 @@
+class Sound:
+    def make_sound(self):
+        print("Animal Sounds")
 
+class Lion(Sound):
+    def make_sound(self):
+        print("Roaringgggggggggggg")
+
+class Cat(Sound):
+    def make_sound(self):
+        print("Meowwwwwwwwwwww")
+
+t1=Cat()
+t1.make_sound()
+
+t2 = Lion()
+t2.make_sound()
+
+t3 = Sound()
+t3.make_sound()
